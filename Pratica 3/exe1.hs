7f4506c451fc7dcda3ba37220304cdba1e4b4419
pdrhenriqueu <43590798+pdrhenriqueu@users.noreply.github.com>
2020-09-02 08:55:37 -0300

    Update exe1.hs

--- Pratica 3/exe1.hs
+++ Pratica 3/exe1.hs
@@ -1,35 +1,22 @@
-Exercicio 3
+--Exercicio 1
+(||) ::Bool->Bool->Bool
+False || False = False
+True || True = True
+False || True = True
+True || False = True
 
-Prelude> 1:[2,3,4]
-[1,2,3,4]
-Prelude> 'a':['b','c','d']
-"abcd"
-Prelude>
-Prelude> head [1,2,3]
-1
-Prelude> tail [1,2,3]
-[2,3]
-Prelude> [1,5,2,3]!!1
-5
-Prelude> [1,5,2,3]!!3
-3
-Prelude> take 2 [1,5,2,3,7]
-[1,5]
-Prelude> drop 2 [1,5,2,3,7]
-[2,3,7]
-Prelude> [1,2] ++ [3,4]
-[1,2,3,4]
-Prelude> [1..10]
-[1,2,3,4,5,6,7,8,9,10]
-Prelude> [7,6..3]
-[7,6,5,4,3]
-Prelude> ['b'..'g']
-"bcdefg"
-Prelude> take 5 [1,3..]
-[1,3,5,7,9]
-Prelude> sum [1..10]
-55
-Prelude> maximum [1,5,2,3,7]
-7
-Prelude> minimum [1,5,2,3,7]
-1
+(||) ::Bool->Bool->Bool
+False || False = False
+_|| _  = True
+
+(||) ::Bool->Bool->Bool
+False || b = b
+True  || _ = True
+
+ou::Bool->Bool->Bool
+ou (x,y)
+ |x == False || y == False = False
+ |otherwise = True
+
+ou (x,y) = if x == False || y == False then False
+          else True
